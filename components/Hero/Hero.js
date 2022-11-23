8db64e9e28cafa27a7762c87d5666a4bd3917c81
steve <thuqpalmer@gmail.com>
2022-11-23 09:20:53 +0000

    feat:Update with magna-images

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import heroStyles from "./Hero.module.css";
+// import { useState } from "react";
+import hero from "../../public/hero.png";
+import heroTwo from "../../public/hero2.png";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
-  const text = `text-[10em] font-bold uppercase`;
-  const flex = "flex items-center justify-center";
-  const images = [
-    "https://res.cloudinary.com/devsteveserver/image/upload/v1658860631/sample.jpg",
-    "https://res.cloudinary.com/devsteveserver/image/upload/v1658860660/cld-sample.jpg",
-  ];
+  const images = [heroTwo, hero];
   const [num, setNum] = useState(0);
-  const style = {
-    backgroundImage: `url(${images[num]})`,
-    backgroundClip: "text",
-    WebkitBackgroundClip: "text",
-    color: "transparent",
-    WebkitTextFillColor: "transparent",
-  };
+
   const toggle = () => {
     if (num < images.length - 1) {
       setNum((prev) => prev + 1);
@@ -24,18 +17,16 @@ const Hero = () => {
     }
   };
   return (
-    <div className="w-screen">
+    <div className=" flex items-center justify-center transition-shadow">
       <div
-        style={style}
-        className={`${heroStyles.clip} ${text} ${flex} object-cover bg-inherit h-[85vh]`}
-      >
-        <span
-          onMouseOver={toggle}
-          className={`cursor-default ${flex} p-0 m-0 object-contain `}
-        >
-          magna
-        </span>
-      </div>
+        onMouseOver={toggle}
+        className=" absolute top-[1.13em] h-[175px] w-[3.4em]  text-[15em] font-bold uppercase "
+      ></div>
+      <Image
+        src={images[num]}
+        alt="name"
+        className={`h-screen bg-cover object-cover duration-200`}
+      />
     </div>
   );
 };
