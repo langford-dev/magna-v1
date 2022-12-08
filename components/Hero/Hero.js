bc05fc4b5403cf5a414ec347f5d7232407c6d5ed
steve <thuqpalmer@gmail.com>
2022-12-08 18:26:29 +0000

    Added Mobile banner

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import hero from "../../public/hero.png";
 import heroTwo from "../../public/hero2.png";
 import heroOne from "../../public/hero1.png";
+import heroMobile from "../../public/heroMobile.png"
 
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -9,13 +10,13 @@ const Hero = () => {
   const images = [hero, heroOne, heroTwo];
   const [num, setNum] = useState(0);
 
-  const toggle = () => {
-    if (num < images.length - 1) {
-      setNum((prev) => prev + 1);
-    } else {
-      setNum(0);
-    }
-  };
+  // const toggle = () => {
+  //   if (num < images.length - 1) {
+  //     setNum((prev) => prev + 1);
+  //   } else {
+  //     setNum(0);
+  //   }
+  // };
   const styles = {
     fab: `fab cursor-pointer bg-[#C13341] w-[40px] h-[40px] rounded-[12px] flex items-center justify-center fixed bottom-[100px] right-[30px]`,
   };
@@ -24,10 +25,16 @@ const Hero = () => {
     <div className="flex items-center justify-center transition-shadow">
       <div className="absolute top-[1.4em]  h-[175px] w-[4em]  text-[15em] font-bold uppercase"></div>
       <Image
-        onMouseOver={toggle}
-        src={images[num]}
+        // onMouseOver={toggle}
+        src={heroOne}
         alt="name"
-        className={`h-screen w-screen object-contain`}
+        className={`hidden h-screen w-screen object-contain md:block`}
+      />
+      <Image
+        // onMouseOver={toggle}
+        src={heroMobile}
+        alt="name"
+        className={`md:hidden h-screen w-screen object-contain block`}
       />
       <div className={styles.fab}>d</div>
     </div>
