18973d66cfcd858cb4bd980e14ba621a21b705d1
steve <thuqpalmer@gmail.com>
2022-12-09 17:20:39 +0000

    [Add] Teams section

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+export const members = [
+  {
+    id: 1,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+  {
+    id: 2,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+  {
+    id: 3,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+  {
+    id: 4,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+  {
+    id: 5,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+  {
+    id: 6,
+    imageUrl: `https://res.cloudinary.com/devsteveserver/image/upload/v1659019979/profile_loza8q.png`,
+    name: `Stephen`,
+  },
+];
