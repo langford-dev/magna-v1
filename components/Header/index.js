d688130cee8dd0cf20b9846c9c3813d6edc0bac5
steve <thuqpalmer@gmail.com>
2022-12-23 17:43:44 +0000

    [fix]:dojo image and refactoring

--- components/Header/index.js
+++ components/Header/index.js
@@ -5,44 +5,48 @@ import Dropdown from "./Dropdown";
 import { useState, useEffect } from "react";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState(false);
+  const links = [
+    {
+      to: "/",
+      path: "/",
+      title: "HOME",
+    },
+    {
+      to: "/about",
+      path: "/about",
+      title: "ABOUT",
+    },
+  ];
+  const [menu, setMenu] = useState(true);
 
   const showMenu = () => {
     setMenu((prev) => !prev);
   };
   const router = useRouter();
   useEffect(() => {
-    // This effect will be called whenever the route changes
-    setMenu(false);
+    setMenu(true);
   }, [router]);
 
   return (
-    <header className="fixed w-screen p-4">
-      <div className="m-auto flex max-w-[1800px] items-center justify-between">
+    <header className={styles.header}>
+      <div className={styles.container}>
         <Link href="/">
-          <div className="rounded-lg bg-[#C13341] p-5 py-2 text-2xl font-bold text-[#fff]">
-            Magna
-          </div>
+          <div className={styles.logo}>Magna</div>
         </Link>
-        <nav className="hidden items-center py-3 sm:flex">
-          <Link href="/">
-            <p
-              className={
-                router.pathname === "/" ? styles.activeNav : styles.navLink
-              }
-            >
-              HOME
-            </p>
-          </Link>
-          <Link href="/about">
-            <p
-              className={
-                router.pathname === "/about" ? styles.activeNav : styles.navLink
-              }
-            >
-              ABOUT
-            </p>
-          </Link>
+        <nav className={styles.nav}>
+          {links.map((link) => (
+            <Link key={link.toString()} href={link.to}>
+              <p
+                className={
+                  router.pathname === link.path
+                    ? styles.activeNav
+                    : styles.navLink
+                }
+              >
+                {link.title}
+              </p>
+            </Link>
+          ))}
           <div className={styles.navLink}>
             <div className="mr-1">
               <Image src="/assets/lock.svg" width={17} height={17} alt="lock" />
@@ -53,7 +57,7 @@ const Navbar = () => {
         <div className={styles.menuBtn}>
           <Image
             onClick={showMenu}
-            src={menu ? "/assets/close.svg" : "/assets/menu.svg"}
+            src={menu ? "/assets/menu.svg" : "/assets/close.svg"}
             width="40"
             height="40"
             alt="menu button"
@@ -67,8 +71,12 @@ const Navbar = () => {
 };
 
 const styles = {
-  navLink: `text-[#fff] hover:text-[#C13341] m-2 mt-0 h-max w-max flex items-center justify-center bg-[#ffffff12] hover:bg-[#A3B1AD2B] cursor-pointer transition-all rounded-md p-5 py-2 cursor-none`,
-  activeNav: `hover:text-[#fff] text-[#C13341] m-2 mt-0 h-max w-max flex items-center justify-center hover:bg-[#ffffff12] bg-[#A3B1AD2B] cursor-pointer transition-all rounded-md p-5 py-2 cursor-none `,
+  header: `fixed w-screen p-4`,
+  container: `m-auto flex max-w-[1800px] items-center justify-between`,
+  logo: `rounded-lg bg-[#C13341] p-5 py-2 text-2xl font-bold text-[#fff]`,
+  nav: `hidden items-center py-3 sm:flex`,
+  navLink: `text-[#fff] hover:text-[#C13341] m-2 mt-0 h-max w-max flex items-center justify-center bg-[#ffffff12] hover:bg-[#A3B1AD2B] transition-all rounded-md p-5 py-2 cursor-none`,
+  activeNav: `hover:text-[#fff] text-[#C13341] m-2 mt-0 h-max w-max flex items-center justify-center hover:bg-[#ffffff12] bg-[#A3B1AD2B] transition-all rounded-md p-5 py-2 cursor-none `,
   menuBtn: `bg-[#ffffff12] px-3 block sm:hidden cursor-none`,
 };
 
